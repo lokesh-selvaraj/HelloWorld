b74ec0cdb3d83376ac3b00c23201e610338d6090
lokesh-selvaraj <loselvar@hotmail.com>
2020-06-18 14:14:38 +0200

    Added welcome message

--- com/test_project/Main.java
+++ com/test_project/Main.java
@@ -2,7 +2,8 @@ package com.test_project;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
     }
 }
