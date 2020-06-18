c0dbe067601a23e1fa81eee11d966d364b12bc1f
lokesh-selvaraj <loselvar@hotmail.com>
2020-06-19 00:30:26 +0200

    Fixed minor bug

--- com/test_project/FizzBuzz.java
+++ com/test_project/FizzBuzz.java
@@ -6,16 +6,22 @@ public class FizzBuzz {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Number : ");
+        System.out.print("Number : ");
         int number = scanner.nextInt();
 
         if(number%5==0 && number%3==0)
             System.out.println("FizzBuzz");
-        if(number%5==0)
+        else if(number%5==0)
             System.out.println("Fizz");
         else if(number%3==0)
             System.out.println("Buzz");
         else
             System.out.println(number);
     }
+
+
+    // OPEN Terminal
+    // cd till 'test_project' , type "javac FizzBuzz.java"
+    // type "cd .." (till 'src' folder)
+    // type "java com.test_project.FizzBuzz"
 }
