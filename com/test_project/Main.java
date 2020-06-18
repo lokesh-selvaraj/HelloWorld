a99d7b8205f4ad5727e09b1e524fc524f4aa54c0
lokesh-selvaraj <loselvar@hotmail.com>
2020-06-18 17:14:16 +0200

    Added Arrays example

--- com/test_project/Main.java
+++ com/test_project/Main.java
@@ -1,6 +1,7 @@
 package com.test_project;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -16,13 +17,13 @@ public class Main {
 
 
         //reference types
-        Date now = new Date();
+        Date now = new Date();                 //java.util.Date
         long currTime = now.getTime();
 
         System.out.println(currTime);
         System.out.println(now);
 
-        Point pt1 = new Point(1, 1);
+        Point pt1 = new Point(1, 1);    //java.awt.*
         Point pt2 = pt1;
         pt1.x = 2;
         System.out.println(pt1);
@@ -38,6 +39,28 @@ public class Main {
         System.out.println(message.toUpperCase());
         System.out.println(message);             // Means any method that modifies a string, will return a new copy of that string
                                                  // making the original version immutable i.e. non-changeable
+
+
+        // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        //1-d arrays
+        int[] numbers = new int[6];
+        numbers[0] = 100;
+        numbers[1] = 200;
+        System.out.println(numbers); // returns [I@7b23ec81
+
+        String properArray = Arrays.toString(numbers);                      //java.util.Arrays
+        System.out.println(properArray); //returns [100, 200, 0, 0, 0, 0]
+
+        //multi-dim arrays
+        int[][] numbers_mulArr = new int[2][3];
+        numbers_mulArr[0][0] = 1;
+        System.out.println(Arrays.toString(numbers_mulArr)); //[[I@6acbcfc0, [I@5f184fc6]
+        System.out.println(Arrays.deepToString(numbers_mulArr)); //[[1, 0, 0], [0, 0, 0]]
+
+        //Other way
+        int[][] numbers_mulArr1 = { {1,2,3}, {4,5,6}  };
+        System.out.println(Arrays.deepToString(numbers_mulArr1));
 
 
 
