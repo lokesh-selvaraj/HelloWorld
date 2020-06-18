c34f965438deefaa5115666f4d68d27fdb720a34
lokesh-selvaraj <loselvar@hotmail.com>
2020-06-18 16:26:13 +0200

    Added primitive vs ref example

--- com/test_project/Main.java
+++ com/test_project/Main.java
@@ -1,9 +1,27 @@
 package com.test_project;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        //primitive types
+        int myAge = 30;
+        int herAge = myAge;
+
+        System.out.println(herAge);
+
+
+
+        //reference types
+        Date now = new Date();
+        long currTime = now.getTime();
+
+        System.out.println(currTime);
+        System.out.println(now);
+
+
+        //SHORTCUT KEY -> sout + Tab = System.out.println();
     }
 }
