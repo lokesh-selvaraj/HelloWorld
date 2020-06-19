2a9f3f561be04daa83d64d4b9cbfbcb1322e7da5
lokesh-selvaraj <loselvar@hotmail.com>
2020-06-19 09:21:31 +0200

    Added checks in MortgageCalculator program

--- com/test_project/Program.java
+++ com/test_project/Program.java
@@ -9,14 +9,41 @@ public class Program {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0.0F;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal : ");             int principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate : ");  float annualInterest = scanner.nextFloat();
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        while(true) {
+            System.out.print("Principal : ");   principal = scanner.nextInt();
 
-        System.out.print("Period (Years) : ");        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+            if(principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 & 1_000_000");
+        }
+
+        while(true){
+            System.out.print("Annual Interest Rate : ");  float annualInterest = scanner.nextFloat();
+
+            if(annualInterest >=1 && annualInterest <=30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        while(true){
+            System.out.print("Period (Years) : ");        byte years = scanner.nextByte();
+
+            if(years >=1 && years <= 30){
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+
 
         double mortgage = principal
                                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
